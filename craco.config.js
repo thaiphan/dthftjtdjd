9ef1d9516e35c6781394d1d1f2683a0b4bb5de67
Thai Phan <tvphan360@gmail.com>
2024-02-10 23:43:36 +1100

    Blah

--- craco.config.js
+++ craco.config.js
@@ -3,14 +3,16 @@ const { ModuleFederationPlugin } = require("webpack").container;
 module.exports = {
   webpack: {
     configure(config) {
+      config.output.publicPath = process.env.REACT_APP_PUBLIC_PATH;
       config.plugins.push(
         new ModuleFederationPlugin({
           name: "settings",
-          library: { type: "var", name: "settings" },
           filename: "remoteEntry.js",
           exposes: {
             "./App": "./src/App",
           },
+          shared:
+            process.env.NODE_ENV === "production" ? ["react", "react-dom"] : [],
         })
       );
       return config;
